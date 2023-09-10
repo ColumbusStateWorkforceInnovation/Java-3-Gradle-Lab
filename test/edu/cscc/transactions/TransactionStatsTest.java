c6d14dfad180e69779245cd022f3f625742c1440
Jim Kirkbride <jkirkbride@cscc.edu>
2023-09-10 07:56:53 -0400

    Add tests around TransactionStats.getTotalRentals()

--- test/edu/cscc/transactions/TransactionStatsTest.java
+++ test/edu/cscc/transactions/TransactionStatsTest.java
@@ -3,6 +3,7 @@ package edu.cscc.transactions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -13,6 +14,17 @@ class TransactionStatsTest {
         TransactionStats transactionStats = new TransactionStats(new ArrayList<>());
 
         assertEquals(0, transactionStats.totalRentals());
+        assertEquals(0, new TransactionStats().totalRentals());
+    }
+
+    @Test
+    public void itReturnsTheTotalRentalsWhenTheTransactionListIsNotEmpty() {
+        List<Transaction> transactions = new ArrayList<>();
+        transactions.add(TransactionBuilder.create().build());
+        transactions.add(TransactionBuilder.create().build());
+        TransactionStats transactionStats = new TransactionStats(transactions);
+
+        assertEquals(2, transactionStats.totalRentals());
     }
 
 }
